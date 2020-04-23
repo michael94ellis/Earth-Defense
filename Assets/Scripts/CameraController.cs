bf8c881569dd0221969c40cf2fb636893912dc6a
Michael Ellis <michael94ellis@gmail.com>
2020-04-23 15:05:19 -0400

    latest

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,42 +9,42 @@ public class CameraController : MonoBehaviour
     float zoomSpeed = 0.2f;
     float boundary = 800.0f;
 
-    public Vector3 origin = new Vector3(0, 0, 0);
     void Start()
     {
-        this.transform.LookAt(origin);
+        this.transform.LookAt(Vector3.zero);
     }
+
     void Update()
     {
         // MARK: - Keyboard Controls
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.RotateAround(origin, Vector3.up, mainSpeed);
+            transform.RotateAround(Vector3.zero, Vector3.up, mainSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.RotateAround(origin, Vector3.down, mainSpeed);
+            transform.RotateAround(Vector3.zero, Vector3.down, mainSpeed);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.RotateAround(origin, Vector3.right, mainSpeed);
+            transform.RotateAround(Vector3.zero, Vector3.right, mainSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.RotateAround(origin, Vector3.left, mainSpeed);
+            transform.RotateAround(Vector3.zero, Vector3.left, mainSpeed);
         }
         if (Input.GetKey(KeyCode.E) && transform.position.y < boundary)
         {
             transform.Translate(Vector3.forward * zoomSpeed);
             if (zoomSpeed >= 0.001f)
-                zoomSpeed = 0.01f * Vector3.Distance(origin, transform.position);
+                zoomSpeed = 0.01f * Vector3.Distance(Vector3.zero, transform.position);
         }
         if (Input.GetKey(KeyCode.Q) && transform.position.y > -boundary)
         {
             zoomSpeed = 0.1f;
             transform.Translate(Vector3.back * mainSpeed);
         }
-        this.transform.LookAt(origin);
+        this.transform.LookAt(Vector3.zero);
     }
 }
