9e4bcf75d4fa55b9b797af2ed3da2e3b702c4a5b
Michael Ellis <michael94ellis@gmail.com>
2020-04-26 01:20:59 -0400

    camera enhance for placement of objects

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -22,36 +22,38 @@ public class CameraController : MonoBehaviour
         this._Xform_Parent = this.transform.parent;
     }
 
-
     void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            CameraDisabled = !CameraDisabled;
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //    CameraDisabled = !CameraDisabled;
         
-        if (!CameraDisabled){
-            if (Input.GetMouseButton(1)){
-                if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0){
+        //if (!CameraDisabled)
+        //{
+            if (Input.GetMouseButton(0) || (Input.GetMouseButton(1) && Earth.GameManager.isPickingLocation))
+            {
+                if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+                {
                     _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
                     _LocalRotation.y -= Input.GetAxis("Mouse Y") * MouseSensitivity;
                     //To not flip camera over top
                     _LocalRotation.y = Mathf.Clamp(_LocalRotation.y, -90f, 90f);
                 }
             }
-            if (Input.GetAxis("Mouse ScrollWheel")!= 0f){
+            else if (Input.GetAxis("Mouse ScrollWheel")!= 0f)
+            {
                 float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitivity;
 
                 ScrollAmount *= (this._CameraDistance * 0.3f);
                 this._CameraDistance += ScrollAmount * -1f;
                 this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.75f, 10f);
             }
-        }
-        
+        //}
 
          //actual camera rig transform
         Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
-        this._Xform_Parent.rotation = Quaternion.Lerp(this._Xform_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
+        this._Xform_Parent.rotation = Quaternion.Lerp(this._Xform_Parent.rotation, QT, OrbitDampening);
         if(this._Xform_Camera.localPosition.z != this._CameraDistance * -1f){
-            this._Xform_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._Xform_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
+            this._Xform_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._Xform_Camera.localPosition.z, this._CameraDistance * -1f, ScrollDampening));
         }
         
     }
--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -6,9 +6,16 @@ public class Orbit : MonoBehaviour
 {
     public Vector3 orbitAngle = new Vector3(0, 1, 0.001f);
 
+    GameObject sun;
+
+    void Start()
+    {
+        sun = GameObject.Find("Sun");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, orbitAngle, Time.deltaTime);
+        transform.RotateAround(sun.transform.position, orbitAngle, Time.deltaTime);
     }
 }
