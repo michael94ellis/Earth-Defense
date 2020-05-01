a15cd9f692d5fb79a4a3867ef989e22486894224
Elliot <elliotmrgn@gmail.com>
2020-05-01 09:12:58 -0400

    added style to new menu

--- Assets/Scripts/BuildMenu.cs
+++ Assets/Scripts/BuildMenu.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuyLaser : MonoBehaviour
+public class BuildMenu : MonoBehaviour
 {
+    public GameObject Panel;
     public bool isPickingLocation = false;
     Earth earth;
 
@@ -24,9 +25,30 @@ public class BuyLaser : MonoBehaviour
             return;
         }
     }
+
+    public void OpenMenu()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+
     public void BuyLaserTurret()
     {
         isPickingLocation = true;
         earth.BuildNewLaserWeapon();
+    }
+
+    public void SendAlienWave()
+    {
+        AlienSpawner.BeginInvasion();
+    }
+
+    public void BuyMissileSiloButton()
+    {
+        isPickingLocation = true;
+        earth.BuildNewMissileSilo();
     }
 }
