1df8912b52e7c5088b4f5fd90ef9e7279952c609
Michael Ellis <michael94ellis@gmail.com>
2020-05-02 22:51:37 -0400

    enhance buildmenu

--- Assets/Scripts/BuildMenu.cs
+++ Assets/Scripts/BuildMenu.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BuildMenu : MonoBehaviour
@@ -8,11 +8,22 @@ public class BuildMenu : MonoBehaviour
     public GameObject Panel;
     private bool isPickingLocation = false;
     Earth earth;
+    Object CityRef;
+    Object GeneratorRef;
+    Object MissileSiloRef;
+    Object LaserTurretRef;
+    //Object SatelliteRef;
+    GameObject PurchasedZoneBuilding;
 
     void Start()
     {
         //needed to initialize earth at start
         earth = GameObject.Find("Earth").GetComponent<Earth>();
+        CityRef = Resources.Load("City");
+        LaserTurretRef = Resources.Load("Turret");
+        MissileSiloRef = Resources.Load("MissileSilo");
+        GeneratorRef = Resources.Load("Generator");
+        //SatelliteRef = Resources.Load("EarthSatellite");
     }
 
     void Update()
@@ -21,14 +32,40 @@ public class BuildMenu : MonoBehaviour
         //TO DO: need a check for if the click is an approved placement
         if (isPickingLocation)
         {
-            earth.DisplayNewObject();
-            if (Input.GetMouseButton(0))
+            RaycastHit[] hitsInOrder = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition)).OrderBy(h => h.distance).ToArray();
+            EarthZone selectedZone = null;
+            foreach (RaycastHit hit in hitsInOrder)
             {
-                isPickingLocation = false;
-                //turns colliders back off
-                foreach (EarthZone zone in Earth.ControlledZones)
+                // This hit.point is the point on earth where you clicked
+                foreach (EarthZone controlledZone in Earth.ControlledZones)
                 {
-                    zone.GetComponent<Collider>().enabled = false;
+                    if (selectedZone == null && hit.collider == controlledZone.GetComponent<Collider>())
+                    {
+                        if (PurchasedZoneBuilding.GetComponent<ShieldGenerator>() != null && controlledZone.ShieldGenerator != null)
+                            return;
+                        else
+                            selectedZone = controlledZone;
+                    }
+                    else if (selectedZone != null && hit.transform.gameObject == this.gameObject && controlledZone.GetComponent<Collider>().bounds.Contains(hit.point))
+                    {
+                        PurchasedZoneBuilding.GetComponent<ZoneBuilding>().ParentZone = selectedZone;
+                        PurchasedZoneBuilding.transform.SetParent(controlledZone.transform, true);
+                        // Get a point directly above the city away from earth
+                        Vector3 awayFromEarth = hit.point - transform.position;
+                        // assign the up vector for the city
+                        PurchasedZoneBuilding.transform.up = awayFromEarth;
+                        PurchasedZoneBuilding.transform.position = hit.point;
+                        if (Input.GetMouseButton(0))
+                        {
+                            isPickingLocation = false;
+                            //turns colliders back off
+                            foreach (EarthZone zone in Earth.ControlledZones)
+                            {
+                                zone.GetComponent<Collider>().enabled = false;
+                            }
+                        }
+                        return;
+                    }
                 }
             }
         }
@@ -44,11 +81,11 @@ public class BuildMenu : MonoBehaviour
         }
     }
 
-    //-----New Buildings-----
+    //-----New Weapon ZoneBuildings-----
     public void BuyLaserTurret()
     {
         isPickingLocation = true;
-        earth.BuildNewLaserWeapon();
+        PurchasedZoneBuilding = BuildNewLaserWeapon();
     }
 
 
@@ -56,7 +93,23 @@ public class BuildMenu : MonoBehaviour
     {
         ActivateZoneColliders();
         isPickingLocation = true;
-        earth.BuildNewMissileSilo();
+        PurchasedZoneBuilding = BuildNewMissileSilo();
+    }
+    //----------------------
+
+    //-----New Defensive ZoneBuildings-----
+    public void BuyMinorCity()
+    {
+        isPickingLocation = true;
+        PurchasedZoneBuilding = BuildNewCity();
+    }
+
+
+    public void BuyShieldGenerator()
+    {
+        ActivateZoneColliders();
+        isPickingLocation = true;
+        PurchasedZoneBuilding = BuildNewShieldGenerator();
     }
     //----------------------
 
@@ -73,5 +126,41 @@ public class BuildMenu : MonoBehaviour
     public void SendAlienWave()
     {
         AlienSpawner.BeginInvasion();
+    }
+
+
+    //-----Build Objects------
+
+    GameObject BuildNewCity()
+    {
+        GameObject newCity = Instantiate(CityRef) as GameObject;
+        return HandleNewObject(newCity, new Vector3(0.0025f, 0.0025f, 0.0025f));
+    }
+
+    GameObject BuildNewLaserWeapon()
+    {
+        GameObject NewWeapon = Instantiate(LaserTurretRef) as GameObject;
+        return HandleNewObject(NewWeapon, new Vector3(0.01f, 0.01f, 0.01f));
+    }
+
+    GameObject BuildNewMissileSilo()
+    {
+        GameObject NewWeapon = Instantiate(MissileSiloRef) as GameObject;
+        return HandleNewObject(NewWeapon, new Vector3(0.01f, 0.01f, 0.01f));
+    }
+
+    GameObject BuildNewShieldGenerator()
+    {
+        GameObject NewShieldGenerator = Instantiate(GeneratorRef) as GameObject;
+        return HandleNewObject(NewShieldGenerator, new Vector3(0.01f, 0.01f, 0.01f));
+    }
+
+    GameObject HandleNewObject(GameObject newObj, Vector3 scale)
+    {
+        PurchasedZoneBuilding = newObj;
+        // Make the new city a child object so it lives inside the earth's coordinate space
+        PurchasedZoneBuilding.transform.SetParent(transform, true);
+        PurchasedZoneBuilding.transform.localScale = scale;
+        return PurchasedZoneBuilding;
     }
 }
--- Assets/Scripts/Earth.cs
+++ Assets/Scripts/Earth.cs
@@ -12,11 +12,6 @@ public interface Damageable
 public class Earth : MonoBehaviour
 {
     MenuManager GameManager;
-    Object CityRef;
-    Object GeneratorRef;
-    Object LaserTurretRef;
-    //Object SatelliteRef;
-    Object MissileSiloRef;
 
     // This holds an item that was just purchased so it can be placed on the earth
     public GameObject NewObject;
@@ -36,90 +31,12 @@ public class Earth : MonoBehaviour
     void Start()
     {
         GameManager = gameObject.GetComponent<MenuManager>();
-        CityRef = Resources.Load("City");
-        LaserTurretRef = Resources.Load("Turret");
-        MissileSiloRef = Resources.Load("MissileSilo");
-        GeneratorRef = Resources.Load("Generator");
-        //SatelliteRef = Resources.Load("EarthSatellite");
         foreach (GameObject earthZone in GameObject.FindGameObjectsWithTag("Player"))
         {
             EarthZone zone = earthZone.GetComponent<EarthZone>();
             if (zone != null)
                 ControlledZones.Add(zone);
         }
-    }
-
-    void Update()
-    {
-        /*if (GameManager.isPickingLocation)
-        {
-            DisplayNewObject();
-            return;
-        }*/
-    }
-
-    public void DisplayNewObject()
-    {
-        RaycastHit[] hitsInOrder = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition)).OrderBy(h => h.distance).ToArray();
-        EarthZone selectedZone = null;
-        foreach (RaycastHit hit in hitsInOrder)
-        {
-            // This hit.point is the point on earth where you clicked
-            foreach (EarthZone controlledZone in ControlledZones)
-            {
-                if (selectedZone == null && hit.collider == controlledZone.GetComponent<Collider>())
-                {
-                    if (NewObject.GetComponent<ShieldGenerator>() != null && controlledZone.ShieldGenerator != null)
-                        return;
-                    else
-                        selectedZone = controlledZone;
-                }
-                else if (selectedZone != null && hit.transform.gameObject == this.gameObject && controlledZone.GetComponent<Collider>().bounds.Contains(hit.point))
-                {
-                    NewObject.GetComponent<ZoneBuilding>().ParentZone = selectedZone;
-                    NewObject.transform.SetParent(controlledZone.transform, true);
-                    // Get a point directly above the city away from earth
-                    Vector3 awayFromEarth = hit.point - transform.position;
-                    // assign the up vector for the city
-                    NewObject.transform.up = awayFromEarth;
-                    NewObject.transform.position = hit.point;
-                    return;
-                }
-            }
-        }
-    }
-
-    public GameObject BuildNewCity()
-    {
-        GameObject newCity = Instantiate(CityRef) as GameObject;
-        return HandleNewObject(newCity, new Vector3(0.0025f, 0.0025f, 0.0025f));
-    }
-
-    public GameObject BuildNewLaserWeapon()
-    {
-        GameObject NewWeapon = Instantiate(LaserTurretRef) as GameObject;
-        return HandleNewObject(NewWeapon, new Vector3(0.01f, 0.01f, 0.01f));
-    }
-
-    public GameObject BuildNewMissileSilo()
-    {
-        GameObject NewWeapon = Instantiate(MissileSiloRef) as GameObject;
-        return HandleNewObject(NewWeapon, new Vector3(0.01f, 0.01f, 0.01f));
-    }
-
-    public GameObject BuildNewShieldGenerator()
-    {
-        GameObject NewShieldGenerator = Instantiate(GeneratorRef) as GameObject;
-        return HandleNewObject(NewShieldGenerator, new Vector3(0.01f, 0.01f, 0.01f));
-    }
-
-    private GameObject HandleNewObject(GameObject newObj, Vector3 scale)
-    {
-        NewObject = newObj;
-        // Make the new city a child object so it lives inside the earth's coordinate space
-        NewObject.transform.SetParent(transform, true);
-        NewObject.transform.localScale = scale;
-        return NewObject;
     }
 
     //public void BuildNewEarthSatellite()
